22fe16dcfbe6731315e3188666f573f6b13557c2
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-09-05 14:53:52 +1000

    the gifs are loading and creating cards but it doesn't look right

--- assets/java.js
+++ assets/java.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
                 var gifDiv = $("<li>");
 
                 gifDiv.html("<a>" + gifs[i] + "</a>")
+                gifDiv.attr("searchWord", gifs[i]);
                 $(".searchButtons").append(gifDiv);           
             
             }
@@ -33,39 +34,44 @@ $(document).ready(function(){
         }
     
 
-
     $("#addSearch").on("click", function(){
         var newOption = $("#searchInput").val();
         gifs.push(newOption);
 
-        console.log(newOption)
         loadButtons();
     });
+
+
+    $(".searchButtons").on("click", function(){
+        var keyword = $(this).attr("searchWord");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=Ew082cXq1TZjBFYEChwkSaW7PTDz3gjn&limit=10";
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+              })
+
+              .then(function(response) {
+                
+                  console.log(response)
+                
+                for (var i = 0; i < 11; i++){
+                    var stillImg = response.data[i].images.original_still.url;
+                    var moveImg = response.data[i].url;
+                    var cardDiv = $("<div>");
+
+                    $(cardDiv).addClass("card");
+                    $(cardDiv).html("<img src=" + stillImg + ">");
+                    $(cardDiv).attr("move-still","still");
+                    
+                    $(".card-columns").append(cardDiv);
+
+                }
+    })
     
-// var stillImg = response.data.images.original_still.url;
-//                 var moveImg = response.data.image_original_url;
+    });
                 
-                
-
-
-
-
-
-            
-//             var url = "https://api.giphy.com/v1/gifs/random?api_key=Ew082cXq1TZjBFYEChwkSaW7PTDz3gjn&tag=" + searchWord + "limit=10";
-
-//             $.ajax({
-//                 url: queryURL,
-//                 method: "GET"
-//               })
-
-//               .then(function(reponse) {
-//                   $(".searchButtons").empty();
-
-
-
-               
-
+        
 
     
 
